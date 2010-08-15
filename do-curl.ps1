7295d1dd14eec973389c0f450a599a29f08feb63
Quentin Proust <q.proust@gmail.com>
2010-08-15 13:28:21 +0200

    Modification du script Do-Curl pour prendre en compte les verbes Http et l'authentification. Ajout des scripts wget et touch.

--- do-curl.ps1
+++ do-curl.ps1
@@ -1,25 +1,51 @@
 # Function CURL
 # Used for requesting a web resource.
-
-param 
-(
-    [string]$url = $null
-);
+#
+###########################################################
+# PARAMETERS
+# Url : Url of the request resource
+# Method : Http verb used (GET,POST,PUT,DELETE,HEAD)
+# Auth : If you need to use Http Authentication.
+#        -auth "your-username"
+#        The password is requested by the script latter
+#        so it cannot be read.
+#
 
 function Do-Curl() 
 {
-    param(
-        [string]$url = $null
-    );
+param 
+(
+    [string]$url = $null,
+	[string]$method = "GET",
+	[string]$auth = $null
+);
 
     trap [Exception] { 
         write $("ERREUR : " + $_.Exception.Message); 
         continue; 
     }
+  
+    if($url -ne $null) {
+    
+        # Create a new request
+		$webrequest = [system.Net.HttpWebRequest]::create($url);
+        $webrequest.set_Method($method);
 
-	if($url -ne $null) {
-		$client = $client = (new-object net.webclient)
-		$resource = $client.DownloadString($url)
-		Write $resource
+        if($auth -ne $null) {
+            $pass = Read-Host -assecurestring "Please enter your password"
+            $credentials = new-object System.Net.NetworkCredential ($auth, $pass);
+            $webrequest.set_Credentials($credentials);
+        }
+
+		# Get the response
+		$response = [system.Net.HttpWebResponse] $webrequest.GetResponse();
+
+		# Get the response stream and read it completely
+		$stream = $response.GetResponseStream();
+		$reader = new-object io.StreamReader($stream);
+		$resource = $reader.ReadToEnd();
+
+		# Write response 
+		Write $resource;
 	}
 }
---
+++ do-touch.ps1
@@ -0,0 +1,15 @@
+function do-touch { 
+	param(
+		[string] $fil
+	)
+	
+	if (test-path $fil)
+	{ 
+		Set-ItemProperty -Path $fil -Name LastWriteTime -Value ([DateTime]::Now)
+		“File $fil timestamp updated”; 
+		return;
+	} else {
+		Set-Content -Path ($fil) -Value ($null);
+		“File $fil created”;
+	}
+}
